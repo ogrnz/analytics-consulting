b45d3a56a68e923f8632d571bea0cadc41cce44c
Olivier Grognuz <olivier.grognuz@gmail.com>
2021-03-13 10:39:04 +0100

    first commit

---
+++ A/script.r
@@ -0,0 +1,12 @@
+library("readxl")
+
+clothing <- read_excel("Aclothing.xls")
+hing$Clothing
+clothing$Clothing <- as.factor(clothing$Clothing)
+clothing$Gender <- as.factor(clothing$Clothing)
+clothing$InteractionType <- as.factor(clothing$Clothing)
+
+str(clothing)
+
+attach(clothing)
+boxplot(clot)
